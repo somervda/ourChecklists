996c8f388edc87248061cc613fccef0f25884a4a
David <39265971+somervda@users.noreply.github.com>
2020-05-22 09:25:44 -0400

    wip - storagelist working

--- src/app/shared/storagelist/storagelist.component.ts
+++ src/app/shared/storagelist/storagelist.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from "@angular/core";
 import * as firebase from "firebase/app";
+import { AngularFireStorage } from "@angular/fire/storage";
+import { Observable } from "rxjs";
+import { ConfirmdialogComponent } from "src/app/dialogs/confirmdialog/confirmdialog.component";
+import { MatDialog } from "@angular/material/dialog";
 
 @Component({
   selector: "app-storagelist",
@@ -14,9 +18,10 @@ import * as firebase from "firebase/app";
  */
 export class StoragelistComponent implements OnInit {
   @Input() docId: string;
-  files: string[];
+  files: { name: string; url$: Observable<any> }[];
+  showSpinner = false;
 
-  constructor() {}
+  constructor(private storage: AngularFireStorage, public dialog: MatDialog) {}
 
   ngOnInit(): void {
     // Get a list of files already in the docId fold
@@ -28,7 +33,59 @@ export class StoragelistComponent implements OnInit {
         // r.items.forEach((element) => {
         //   console.log("element", element.name);
         // })
-        (this.files = r.items.map((i) => i.name))
+        (this.files = r.items.map((i) => {
+          return { name: i.name, url$: this.getStorageUrl(i.name) };
+        }))
     );
+  }
+
+  onUploadFile(event) {
+    console.log("onUploadFile", event);
+    const fileToUpload = event.target.files[0];
+    this.showSpinner = true;
+    const task = this.storage
+      .upload(`docs/${this.docId}/${fileToUpload.name}`, fileToUpload)
+      .then((t) => {
+        this.showSpinner = false;
+        this.files.push({
+          name: fileToUpload.name,
+          url$: this.getStorageUrl(fileToUpload.name),
+        });
+      })
+      .catch((e) => (this.showSpinner = false));
+  }
+
+  getStorageUrl(filename: string): Observable<any> {
+    console.log("getStorageUrl", filename);
+    return this.storage.ref(`docs/${this.docId}/${filename}`).getDownloadURL();
+  }
+
+  onDeleteFile(fileName: string) {
+    console.log("onDeleteFile", fileName);
+    const prompt = "Are you sure you want to delete this file: " + fileName;
+    const dialogRef = this.dialog.open(ConfirmdialogComponent, {
+      width: "300px",
+      data: { heading: "Confirm", prompt: prompt },
+    });
+
+    dialogRef.afterClosed().subscribe((choice) => {
+      if (choice) {
+        console.log("we said yes");
+        this.storage
+          .ref(`docs/${this.docId}/${fileName}`)
+          .delete()
+          .toPromise()
+          .then(() => {
+            // clean up files array
+            console.log("filedeleted");
+            const filesIndex = this.files.findIndex((f) => f.name == fileName);
+            console.log("filesIndex", filesIndex);
+            this.files.splice(filesIndex, 1);
+          })
+          .catch(function (error) {
+            console.error("File delete error:", error);
+          });
+      }
+    });
   }
 }
